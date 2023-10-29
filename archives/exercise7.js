6d1d5a47eb778a273ec68e01115eab365e533c8a
Paul Figueroa <pjsfigueroa@gmail.com>
2023-10-29 21:15:48 +0800

    Working state.

--- archives/exercise7.js
+++ archives/exercise7.js
@@ -5,41 +5,50 @@ What is the 10,001st prime number?
 
 /* 
 What is a prime number?
+    A whole number greater than 1 that cannot be exactly divided by 
+    any whole number other than itself and 1 (e.g. 2, 3, 5, 7, 11).
 */
 
-const TARGET = 10001
-let counter = 0
-let number = 1
+let tracker = 0 // prime numbers found
+let testnum = 2 // test each integer starting starting from 2
 
 // Test if the number is a prime number
 function isPrime(number){
+
     let count = 0
+    
     for(let i = 1; i <= number; i++){
-        // if there is no remainder it means that we have found a factor, so we increment the counter by one
+        // if there is no remainder it means that we have found a factor, 
+        // so we increment the counter by one
         number%i === 0 ? count++ : count
-        // no need to loop through all numbers as prime numbers has only two factors, one and itself
+        
+        // no need to loop through all numbers as prime numbers 
+        // has only two factors, one and itself
         if(count > 2) { 
             break
         } 
     }
-    return count > 2 ? `${number} is not prime` : `${number} is prime`
+    // return count > 2 ? `${number} is not prime` : `${number} is prime`
+    return count > 2 ? false : true
 }
 
-console.log(isPrime(4))
 
 // use recursive function
-function prime(){
+function prime(target){
     // base case - stop
-    if(counter === TARGET){
-        return number
+    if(tracker === target){
+       // console.log('base case')
+        console.log(`The ${target} prime number is ${testnum - 1}`)
+        return 
     }
 
-    // recursive case
-    counter++
-    prime()
+    // recursion
+    isPrime(testnum) ? tracker++ : tracker
+    // console.log(target, tracker, testnum)
+    testnum++
+   // console.log(tracker)
+    prime(target)
 }
+console.log(isPrime(7927))
 
-prime()
-
-console.log('Counter', counter)
-
+prime(1001)
