ab230cee84a64e94902d2472534583085b77460b
Paul Figueroa <pjsfigueroa@gmail.com>
2023-11-01 13:25:12 +0800

    Clean up code

--- archives/exercise9.js
+++ archives/exercise9.js
@@ -1,20 +1,24 @@
-/* Special Pythagorean Triplet */
+/* Problem 9: Special Pythagorean Triplet */
 
 /* 
-find the triplet using a random number generator function
-
+Reference
+https://www.youtube.com/watch?v=n6vL2KiWrD4
 Formula
-a = m2 - n2
-b = 2mn
-c = m2 + n2
-where m > n
+    a = m2 - n2
+    b = 2mn
+    c = m2 + n2
+    where m > n
 
+    Find the triplet using a random number generator function
 */
 
+// Generate random number
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Find a combination that will yield the 
+// target value using random number generator
 function generateTriples(m, n){
     if(m > n) {
         let a = m*m - n*n
@@ -25,24 +29,26 @@ function generateTriples(m, n){
     return null
 }
 
-// console.log(generateTriples(getRandomInt(400), getRandomInt(400)))
-
+// Test targets
+// (91, 60, 109) => sum: 260
+// (140, 171, 221) => sum: 532
 let target = 1000
 let sum = 0
 let triplet = []
+
+// find the triple that has a sum equal to "target"
 while (sum != target){
-    triplet = generateTriples(getRandomInt(400), getRandomInt(400))
+    triplet = generateTriples(getRandomInt(target), getRandomInt(target))
     
-    if(triplet != null){
-        sum = triplet.reduce((acc, curr) => acc + curr, 0)
-    }else{
-        sum = 0
-    }
+    triplet != null 
+        ? sum = triplet.reduce((acc, curr) => acc + curr, 0)
+        : sum = 0
+    
 }
 
-console.log('Triplet:', triplet)
-
 const prod = triplet.reduce((acc, curr) => acc * curr)
+console.log('========================================')
+console.log('Triplet:', triplet)
+console.log('Sum: ', sum)
 console.log('Product: ', prod)
-
-
+console.log('========================================')
